e4f744cfe10a82163343c074e75247c242582620
Joachim Werner <j.werner@sal-a.de>
2023-09-25 15:41:20 +0200

    Removed baseUrl

--- assets/ChartJSAsset.php
+++ assets/ChartJSAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 class ChartJSAsset extends AssetBundle
 {
     public $sourcePath = null;
-    public $baseUrl = 'https://cdn.jsdelivr.net/npm/chart.js@3.6.0';
+    // public $baseUrl = 'https://cdn.jsdelivr.net/npm/chart.js@3.6.0';
     public $js = [
         'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.umd.js'
     ];
