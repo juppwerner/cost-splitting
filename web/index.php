213c33e9db4522146e39f2e2f9d08495500f10fe
Joachim Werner <joachim.werner@diggin-data.de>
2023-09-26 21:38:02 +0200

    Added translations

--- web/index.php
+++ web/index.php
@@ -31,6 +31,15 @@ if(file_exists($versionFile)) {
 }
 // (new yii\web\Application($config))->setVersion($version)->run();
 $app = new yii\web\Application($config);
+
+$app->getModule('user')->mailParams = [
+    'fromEmail' => 'no-reply@example.com',
+    'welcomeMailSubject' => Yii::t('app', 'Welcome to {0}', $app->name),
+    'confirmationMailSubject' => Yii::t('app', 'Confirm account on {0}', $app->name),
+    'reconfirmationMailSubject' => Yii::t('app', 'Confirm email change on {0}', $app->name),
+    'recoveryMailSubject' => Yii::t('app', 'Complete password reset on {0}', $app->name),
+    'twoFactorMailSubject' => Yii::t('app', 'Code for two factor authentication on {0}', $app->name),
+];
 $app->setVersion($version);
 $app->run();
 
