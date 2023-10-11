fa660c7e01629bcad9b3c60ceccf5ce20d56591e
Joachim Werner <j.werner@sal-a.de>
2023-10-11 17:06:14 +0200

    Removed redundant comments

--- config/messages.php
+++ config/messages.php
@@ -17,7 +17,7 @@ return [
     'sort' => false,
     // boolean, whether to remove messages that no longer appear in the source code.
     // Defaults to false, which means each of these messages will be enclosed with a pair of '@@' marks.
-    'removeUnused' => false,
+    'removeUnused' => true,
     // array, list of patterns that specify which files/directories should NOT be processed.
     // If empty or not set, all files/directories will be processed.
     // A path matches a pattern if it contains the pattern string at its end. For example,
@@ -48,27 +48,4 @@ return [
     'messagePath' => __DIR__  . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'messages',
     // boolean, whether the message file should be overwritten with the merged messages
     'overwrite' => true,
-
-
-    /*
-    // 'db' output format is for saving messages to database.
-    'format' => 'db',
-    // Connection component to use. Optional.
-    'db' => 'db',
-    // Custom source message table. Optional.
-    // 'sourceMessageTable' => '{{%source_message}}',
-    // Custom name for translation message table. Optional.
-    // 'messageTable' => '{{%message}}',
-    */
-
-    /*
-    // 'po' output format is for saving messages to gettext po files.
-    'format' => 'po',
-    // Root directory containing message translations.
-    'messagePath' => __DIR__ . DIRECTORY_SEPARATOR . 'messages',
-    // Name of the file that will be used for translations.
-    'catalog' => 'messages',
-    // boolean, whether the message file should be overwritten with the merged messages
-    'overwrite' => true,
-    */
 ];
